c340b384f014a418b13d609fe8a437974da6d0ff
Rishal <Rishal.mhd009@gmail.com>
2023-05-29 20:17:02 +0530

    fetch including

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://react-http-bff42-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("sending cart data failed");
+      }
+
+      const responseData = await response.json();
+    };
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
